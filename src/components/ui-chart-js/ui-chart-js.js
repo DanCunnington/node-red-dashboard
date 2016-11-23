1de195e3a6371e61341be9fc211e57f7564d61e5
Dan Cunnington <dancunnington@uk.ibm.com>
2016-11-23 09:53:45 +0000

    removed debug

--- src/components/ui-chart-js/ui-chart-js.js
+++ src/components/ui-chart-js/ui-chart-js.js
@@ -12,8 +12,6 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
 
                     // When new values arrive, update the chart
                     scope.$watch('me.item.value', function (newValue) {
-                        console.log(newValue);
-
                         if (newValue !== undefined && newValue.length > 0) {
                             scope.config.nodata = false;
                             newValue = newValue[0];
